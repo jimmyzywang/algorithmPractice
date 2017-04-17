9d59df4cd23639595d2e99cd6a28539c2ba0415d
王泽一 <jimmyzywang@tencent.com>
2017-04-17 20:22:59 +0800

    init

---
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmPractice
+//
+//  Created by 王泽一 on 2017/4/17.
+//  Copyright © 2017年 jimmyzywang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
