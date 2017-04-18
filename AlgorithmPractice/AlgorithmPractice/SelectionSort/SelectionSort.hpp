1bfef5481f4dc1fa456d8ca0934cede81c0b4945
王泽一 <jimmyzywang@tencent.com>
2017-04-18 10:58:29 +0800

    使用模板函数排序

--- AlgorithmPractice/AlgorithmPractice/SelectionSort/SelectionSort.hpp
+++ AlgorithmPractice/AlgorithmPractice/SelectionSort/SelectionSort.hpp
@@ -14,10 +14,66 @@
 #include <array>
 using namespace std;
 
-void mySelectionSortV1(int arr[],size_t count);
+/*  这样理解不对：
+    选择排序是一种O(N^2)的排序算法，具体情况是这样的：
+    step1: 5 21 10 4 9 (初始状态)
+    step2: 从第一个元素(5)开始，跟它后面的元素依次比较，如果后面的某个元素比它小，那么他们交换位置，本次的结果是 4 21 10 5 9
+    step3: 从第二个元素(21)继续，依次类推 本次的结果是 4 5 10 21 9
+    ...
+*/
+template<typename T>
+void mySelectionSortV1(T arr[],size_t count){
+    for (int i = 0; i < count; i++) {
+        for (int j = i + 1; j < count; j++) {
+            if (arr[j] < arr[i]) {
+                swap(arr[j], arr[i]);
+                break;
+            }
+        }
+    }
+}
 
-void mySelectionSortV2(int arr[],size_t count);
+/*  正确的理解：
+    选择排序是一种O(N^2)的排序算法，具体情况是这样的：
+    step1: 5 21 10 4 9 (初始状态)
+    step2: 从第一个元素(5)开始，跟它后面的“所有”元素依次比较，找到它们中最小的那个，然后交换位置，本次的结果是 4 21 10 5 9
+    step3: 从第二个元素(21)继续，依次类推 本次的结果是 4 5 10 21 9
+    ...
+ 
+    Fix V1:这样的理解不对，比它小的元素不一定是最小的，可能还有更小的，但是这是break的话就错了
+*/
+template<typename T>
+void mySelectionSortV2(T arr[],size_t count){
+    for (int i = 0; i < count; i++) {
+        int miniIndex = i + 1;
+        for (int j = i + 1; j < count; j++) {
+            if (arr[j] < arr[miniIndex]) {
+                miniIndex = j;
+            }
+        }
+    }
+}
 
-void mySelectionSortV3(int arr[],size_t count);
+/*  正确的理解：
+    选择排序是一种O(N^2)的排序算法，具体情况是这样的：
+    step1: 5 21 10 4 9 (初始状态)
+    step2: 从第一个元素(5)开始，跟它后面的“所有”元素依次比较，找到它们中最小的那个，然后交换位置，本次的结果是 4 21 10 5 9
+    step3: 从第二个元素(21)继续，依次类推 本次的结果是 4 5 10 21 9
+    ...
+    
+    Fix V2：临界条件有问题，当外层i = (n - 1)时，miniIndex从n开始，这时候arr[n]就越界了
+*/
+template<typename T>
+void mySelectionSortV3(T arr[],size_t count){
+    for (int i = 0; i < count; i++) {
+        int miniIndex = i; //这里miniIndex从i开始，因为数组的坐标是从0开始的，i就是第i+1个元素
+        for (int j = i; j < count; j++) {
+            if (arr[j] < arr[miniIndex]) {
+                miniIndex = j;
+            }
+        }
+        swap(arr[miniIndex], arr[i]);
+    }
+}
 
 #endif /* SelectionSort_hpp */
--- AlgorithmPractice/AlgorithmPractice/Utility/Utility.hpp
+++ AlgorithmPractice/AlgorithmPractice/Utility/Utility.hpp
@@ -10,10 +10,54 @@
 #define UTILITY_hpp
 
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include <random>
+#include <cassert>
+
 namespace Utility{
-    int* GenerateRamdonArray(size_t count,size_t rangeL,size_t rangeR);
+    int* GenerateRamdonArray(size_t count,size_t rangeL,size_t rangeR){
+        assert(rangeL < rangeR);
+        int* arr = new int[count];
+        srand(time(NULL));
+        for (int i = 0; i < count; i++) {
+            arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+        }
+        return arr;
+    }
     
-    void PrintArray(int array[],size_t count);
+    //打印arr的每一个元素
+    template<typename T>
+    void PrintArray(T array[],size_t count){
+        std::cout << "PrintArray: start" << std::endl;
+        for (int i = 0; i < count; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl << "PrintArray end " << std::endl;
+    }
+    
+    //测试arr是否被正确排序
+    template<typename T>
+    bool isSorted(T arr[],size_t count){
+        for (int i = 0; i < count - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //测试排序的时间
+    template<typename T>
+    void TestSortTime(const std::string& sortName,void(*sort)(T[],size_t),T arr[],size_t count){
+        clock_t start_time = clock();
+        sort(arr,count);
+        clock_t end_time = clock();
+        assert(isSorted(arr, count));
+        std::cout << "Time:" << sortName << (end_time - start_time) / CLOCKS_PER_SEC << "s";
+    }
+    
+
 }
 
 
